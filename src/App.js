ddf29b2dcee86abd41479b6dd7af2752ad6a7450
myommt <zai.myo.mintun@gmail.com>
2025-01-29 07:53:12 -0600

    change something

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>I am happy with Code Pipeline.</h1>
+      <h1>I am happy with Code Pipeline and CloudFormation.</h1>
     </div>
   );
 }
